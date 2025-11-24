7f167c4748c9f448b08293876d5895df7b313c9c
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-11-24 18:34:07 +0100

    fix: footer color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     const currentYear = new Date().getFullYear();
     return (
         <footer className="p-10 max-w-[1300px] w-full mx-auto flex">
-            <span className="w-full border-t border-stone-500 text-stone-500 text-center p-10">&copy; {currentYear} | All rights are reserved | Developed by RykerWilder </span>
+            <span className="w-full border-t border-slate-400 text-slate-400 text-center p-10">&copy; {currentYear} | All rights are reserved | Developed by RykerWilder </span>
         </footer>
     );
 };
